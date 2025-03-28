f6d6562f5b2b7a3444ead8bfbc2599042359c25d
Bhawadharani <dharanibhawa@gmail.com>
2025-03-28 16:24:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base"";
+  base: "",  // Make sure to assign a value to `base`, or leave it empty if not needed
   plugins: [react()],
 })
-
